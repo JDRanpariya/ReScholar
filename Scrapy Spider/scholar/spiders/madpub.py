45457af3c0f8637092cf9196bbc38fca1042ef48
JDRanpariya <51157407+JDRanpariya@users.noreply.github.com>
2021-04-05 11:40:16 +0530

    Update madpub.py

--- Scrapy Spider/scholar/spiders/madpub.py
+++ Scrapy Spider/scholar/spiders/madpub.py
@@ -13,18 +13,20 @@ class PubMadSpider(scrapy.Spider):
         queries = ['weight loss']
         for query in queries:
             url = 'https://pubmed.ncbi.nlm.nih.gov/?' + urlencode({'term': query})
-            yield scrapy.Request(url, callback=self.parse, meta={'position': 0})
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        print(response.url)
-        position = response.meta['position']
-        for res in response.xpath('//*[@data-rp]'):
+
+        for res in response.xpath('//div[@class='search-results-chunks']'):
+            
             link = res.xpath('.//h3/a/@href').extract_first()
+            
             temp = res.xpath('.//h3/a//text()').extract()
             if not temp:
                 title = "[C] " + "".join(res.xpath('.//h3/span[@id]//text()').extract())
             else:
                 title = "".join(temp)
+                
             snippet = "".join(res.xpath('.//*[@class="gs_rs"]//text()').extract()).replace('Â â€¦', '... ')
             cited = res.xpath('.//a[starts-with(text(),"Cited")]/text()').extract_first()
             temp = res.xpath('.//a[starts-with(text(),"Related")]/@href').extract_first()
@@ -33,11 +35,19 @@ class PubMadSpider(scrapy.Spider):
             num_versions_link = "https://scholar.google.com" + temp if temp else ""
             num_versions = res.xpath('.//a[contains(text(),"version")]/text()').extract_first()
             published_data = "".join(res.xpath('.//div[@class="gs_a"]//text()').extract())
-            position += 1
-            item = {'title': title, 'link': link, 'cited': cited, 'relatedLink': related, 'position': position,
-                    'numOfVersions': num_versions,'numOfVersionsLink': num_versions_link , 'publishedData': published_data, 'snippet': snippet}
+
+            
+            item = {
+                'title': title,
+                'link': link,
+                'cited': cited,
+                'relatedLink': related,
+                'position': position,
+                'numOfVersions': num_versions,
+                'numOfVersionsLink': num_versions_link ,
+                'publishedData': published_data,
+                'snippet': snippet
+            }
+            
             yield item
-        next_page = response.xpath('//td[@align="left"]/a/@href').extract_first()
-        if next_page:
-            url = "https://scholar.google.com" + next_page
-            yield scrapy.Request(url, callback=self.parse,meta={'position': position})
+
