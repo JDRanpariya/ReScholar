ca93cb9c0e32e684744ad1117a508d8728934a78
JDRanpariya <51157407+JDRanpariya@users.noreply.github.com>
2021-04-07 17:43:36 +0530

    fixed encoding & added authors, pubs, year, citation link

--- scrapy spiders/scholar/spiders/scholar.py
+++ scrapy spiders/scholar/spiders/scholar.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode
 from urllib.parse import urlparse
 import json
 from datetime import datetime
+import re
 
 class ScholarSpider(scrapy.Spider):
     name = 'scholar'
@@ -25,22 +26,34 @@ class ScholarSpider(scrapy.Spider):
                 title = "[C] " + "".join(res.xpath('.//h3/span[@id]//text()').extract())
             else:
                 title = "".join(temp)
-            # TODO: Snippet and Publishers contain mangled UTF-8 encoding. Fix this using some form of UTF-8 unmangling. 
-            snippet = "".join(res.xpath('.//*[@class="gs_rs"]//text()').extract())
+            # Review: Snippet and Publishers contain mangled UTF-8 encoding. Fix this using some form of UTF-8 unmangling. 
+            snippet = "".join(res.xpath('.//*[@class="gs_rs"]//text()').extract()).replace("\u2026","").replace("\u00a0","")
             citations = res.xpath('.//a[starts-with(text(),"Cited")]/text()').extract_first().replace("Cited by ", "")
-            # TODO: Implement CitationLink
+            
+            # Review: Implement CitationLink
+            temp = res.xpath("//div[@class='gs_fl']/a[3]/@href").extract_first()
+            related_link = "https://scholar.google.com" + temp if temp else ""
+            
             temp = res.xpath('.//a[starts-with(text(),"Related")]/@href').extract_first()
             related_link = "https://scholar.google.com" + temp if temp else ""
+            
             temp = res.xpath(".//a[@class='gs_nph']/@href").extract_first()
             versions_link = "https://scholar.google.com" + temp if temp else ""
+            
             versions_count = res.xpath('.//a[contains(text(),"version")]/text()').extract_first().replace("All ", "").replace(" versions", "")
-            # TODO: Seperate Authors, Year, Publisher
-            publishers = "".join(res.xpath('.//div[@class="gs_a"]//text()').extract())
+            # Review: Seperate Authors, Year, Publisher
+            publishers = "".join(res.xpath('.//div[@class="gs_a"]//text()').extract()).replace("\u2026","").replace("\u00a0","")
+            
+            yearofpub = re.search("\d+", publishers)[0]
+            authors = publishers.split("-")[0]
+            publisher = publishers.split("-")[-1]
+            
             position += 1
             item = {'position': position, 'title': title, 'publishers': publishers, 'snippet': snippet, 'link': link, 
                     'citations': citations, 'relatedLink': related_link, 'versionsCount': versions_count,
                     'versionsLink': versions_link}
             yield item
+            
         next_page = response.xpath('//td[@align="left"]/a/@href').extract_first()
         if next_page:
             url = "https://scholar.google.com" + next_page
