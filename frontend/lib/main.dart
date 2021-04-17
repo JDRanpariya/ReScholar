5f148876f251b1e0850bb73e33dcd5d2542a5999
jdranpariya <jdpatel.code@gmail.com>
2021-04-17 11:25:18 +0530

    added wrapper & auth

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-
-import 'package:rescholar/widgets/navigation_bar.dart';
+import './screens/wrapper.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ReScholar(),
+    home: wrapper(),
     theme: ThemeData(
       brightness: Brightness.dark,
     ),
     themeMode: ThemeMode.dark,
   ));
-}
-
-class ReScholar extends StatefulWidget {
-  @override
-  _ReScholarState createState() => _ReScholarState();
-}
-
-class _ReScholarState extends State<ReScholar> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: NavigationBar());
-  }
 }
