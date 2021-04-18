04ab0ceb1415880681c98f8de4649dfd16652aa9
Pranav Abraham <mailforpranav247@gmail.com>
2021-04-18 12:39:00 +0300

    Added better designed wireframe buttons

--- frontend/lib/screens/Authentication/sign_in.dart
+++ frontend/lib/screens/Authentication/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:rescholar/services/auth.dart';
+// import 'package:rescholar/assets/'
 
 class SignIn extends StatefulWidget {
   @override
@@ -14,48 +15,77 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-        title: Text("Sign In"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            child: Column(
-          children: [
-            SizedBox(height: 20.0),
-            ElevatedButton(
-                child: Text(
-                  "Sign In using Google",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                onTap: () async {
                   dynamic user = await _auth.signInWithGoogle();
                   if (user == null) {
-                    print("error signing in");
+                    print("Error signing in!");
                   } else {
-                    print("User Signed In");
+                    print("User has signed in successfully!");
                     print(user);
                   }
-                }),
-            SizedBox(height: 20.0),
-            Text("OR"),
-            SizedBox(height: 20.0),
-            TextButton(
-              onPressed: () async {
-                dynamic user = await _auth.signInAnon();
-                if (user == null) {
-                  print("error signing in");
-                } else {
-                  print("User Signed In");
-                  print(user);
-                }
-              },
-              child: Text("Sign In as Guest"),
-            ),
-          ],
-        )),
+                },
+                child: Container(
+                    height: 50,
+                    width: 210,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        color: Color(0x404880DE)),
+                    alignment: Alignment.center,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(
+                            image: AssetImage("assets/icons/google_logo.png"),
+                            height: 25.0,
+                            width: 25.0,
+                          ),
+                          Text("Sign in using Google",
+                              style: TextStyle(fontSize: 16))
+                        ])),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("OR",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ))),
+              InkWell(
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                onTap: () async {
+                  dynamic user = await _auth.signInAnon();
+                  if (user == null) {
+                    print("Error signing in!");
+                  } else {
+                    print("User has signed in successfully!");
+                    print(user);
+                  }
+                },
+                child: Container(
+                    height: 50,
+                    width: 190,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        color: Color(0x40FFFFFF)),
+                    alignment: Alignment.center,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.account_circle_rounded, size: 25.0),
+                          Text("Sign in as a Guest",
+                              style: TextStyle(fontSize: 16))
+                        ])),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
