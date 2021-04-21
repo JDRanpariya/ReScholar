70f84c818effa96d3bfb81ee0d6d85a07a14510c
jdranpariya <jdpatel.code@gmail.com>
2021-04-21 10:45:37 +0530

    updated microsoft_academic spider

--- data_sources/microsoft_academic/microsoft_academic.py
+++ data_sources/microsoft_academic/microsoft_academic.py
@@ -1,64 +1,102 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from urllib.parse import urlencode
-from urllib.parse import urlparse
 import json
-from datetime import datetime
 import os
 from dotenv import load_dotenv
+from multiprocessing import Process, Queue, Manager
+import scrapy.spiders
+from scrapy.crawler import CrawlerProcess
+from urllib.parse import urlencode
 
 load_dotenv()
 
 
-class MicrosoftAcademicSpider(scrapy.Spider):
-    name = 'MicrosoftAcademicSpider'
-    allowed_domains = ['api.labs.cognitive.microsoft.com']
+def MicrosoftAcademic(request):
+    def script(queue, output):
+        try:           
+            name = 'MicrosoftAcademicSpider'
+            allowed_domains = ['api.labs.cognitive.microsoft.com']
 
-    def start_requests(self):
-        query = 'residual learning'
-        api_key = os.getenv("MICROSOFT_RESEARCH_API_KEY")
-        url = f'https://api.labs.cognitive.microsoft.com/academic/v1.0/interpret?query={query}&complete=0&count=10&model=latest'
+            def start_requests(self):
+                query = 'residual learning'
+                api_key = os.getenv("MICROSOFT_RESEARCH_API_KEY")
+                url = f'https://api.labs.cognitive.microsoft.com/academic/v1.0/interpret?query={query}&complete=0&count=10&model=latest'
 
-        yield scrapy.Request(url, headers={'Ocp-Apim-Subscription-Key': api_key}, callback=self.parse)
+                yield scrapy.Request(url, headers={'Ocp-Apim-Subscription-Key': api_key}, callback=self.parse)
 
-    def parse(self, response):
+            def parse(self, response):
 
-        res = json.loads(response.body)
-        api_key = os.getenv("MICROSOFT_RESEARCH_API_KEY")
-        expr1 = str(res['interpretations'][0]['rules'][0]
-                    ['output']['value']).replace("//", "")
+                res = json.loads(response.body)
+                api_key = os.getenv("MICROSOFT_RESEARCH_API_KEY")
+                expr1 = str(res['interpretations'][0]['rules'][0]
+                            ['output']['value']).replace("//", "")
 
-        url = f'https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr={expr1}&model=latest&count=10&offset=0&attributes=Ti,S,Y,CC,AA.AuN'
+                url = f'https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr={expr1}&model=latest&count=10&offset=0&attributes=Ti,S,Y,CC,AA.AuN'
 
-        yield scrapy.Request(url, headers={'Ocp-Apim-Subscription-Key': api_key}, callback=self.parse_expr)
+                yield scrapy.Request(url, headers={'Ocp-Apim-Subscription-Key': api_key}, callback=self.parse_expr)
 
-    def parse_expr(self, response):
+            def parse_expr(self, response):
 
-        res = json.loads(response.body)
+                res = json.loads(response.body)
 
-        for i in res['entities']:
+                for i in res['entities']:
 
-            links = []
-            for link in i["S"]:
-                links.append(link["U"])
-            link = links
+                    links = []
+                    for link in i["S"]:
+                        links.append(link["U"])
+                    link = links
 
-            title = i["Ti"]
+                    title = i["Ti"]
 
-            cited = i["CC"]
+                    cited = i["CC"]
 
-            year = i["Y"]
+                    year = i["Y"]
 
-            authors = []
-            for author in i['AA']:
-                authors.append(author["AuN"])
-            published_data = authors
+                    authors = []
+                    for author in i['AA']:
+                        authors.append(author["AuN"])
+                    published_data = authors
 
-            item = {
-                'title': title,
-                'link': link,
-                'cited': cited,
-                'authors': authors,
-                'year': year
-            }
-            yield item
+                    item = {
+                        'title': title,
+                        'link': link,
+                        'cited': cited,
+                        'authors': authors,
+                        'year': year
+                    }
+                    yield item
+
+            # Receive arguments from HTTP request
+            default_query = ['Residual learning']
+            query = request.args.get('q') if request.args.get('q')!=None else default_query
+            item_count = request.args.get('item_count') if request.args.get('item_count')!=None else 10
+            custom_settings = {
+                'CLOSESPIDER_ITEMCOUNT':f'{item_count}',
+                EXTENSIONS = {'scrapy.extensions.closespider.CloseSpider': CLOSESPIDER_ITEMCOUNT}}
+            
+            # Instantiate and run spider
+            process = CrawlerProcess(custom_settings)
+            process.crawl(MicrosoftAcademicSpider, query = query)
+            process.start()
+            
+            queue.put(None)
+
+        # Check for errors in process and add to queue
+        except Exception as e:
+            queue.put(e)         
+
+    queue = Queue()
+    manager = Manager()
+    output = manager.list()
+
+    # Wrapping the spider in a child process
+    main_process = Process(target=script, args=(queue, output,))
+    main_process.start()    
+    main_process.join()     
+
+    # Display error checking results
+    result = queue.get()
+    if result is not None:
+        raise result
+
+    return json.dumps(list(output))
