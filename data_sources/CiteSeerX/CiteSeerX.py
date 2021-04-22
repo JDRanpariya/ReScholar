590924a2304d57cdfe1d214af8f378d72babd2d2
JDRanpariya <51157407+JDRanpariya@users.noreply.github.com>
2021-04-22 09:43:33 +0530

    added support for query args and changed var names

--- data_sources/CiteSeerX/CiteSeerX.py
+++ data_sources/CiteSeerX/CiteSeerX.py
@@ -3,39 +3,39 @@ from bs4 import BeautifulSoup
 import json
 
 def scrape_data_from_div(soup_div):
-    result = {}
+    item = {}
     domain = "https://citeseerx.ist.psu.edu"
 
-    result["title"] = soup_div.find("a").text.strip()
-    result["link"] = domain + soup_div.find("a")['href']
-    result["authors"] = soup_div.find("span", class_="authors").text[2:].strip()
-    result["snippet"] = soup_div.find("div", class_="snippet").text[4:-4].strip()
+    item["title"] = soup_div.find("a").text.strip()
+    item["link"] = domain + soup_div.find("a")['href']
+    item["authors"] = soup_div.find("span", class_="authors").text[2:].strip()
+    item["snippet"] = soup_div.find("div", class_="snippet").text[4:-4].strip()
 
     try: 
-        result["citationLink"] = domain + soup_div.find("a", class_="citation remove")['href']
-        result["citations"] = soup_div.find("a", class_="citation remove").text.split(' ')[2]
+        item["citationLink"] = domain + soup_div.find("a", class_="citation remove")['href']
+        item["citations"] = soup_div.find("a", class_="citation remove").text.split(' ')[2]
     except TypeError:
-        result["citationLink"] = "None"
-        result["citation"] = "None"
+        item["citationLink"] = "None"
+        item["citation"] = "None"
     
-    return result
+    return item
 
 
-def CiteSeerX(search_term):
+def CiteSeerX():
+    default_query="residual learning"
+    query = request.args.get('q') if request.args.get('q')!=None else default_query
+    
+    url = f"https://citeseerx.ist.psu.edu/search?q={query}"
 
-    search_url = "https://citeseerx.ist.psu.edu/search?q=" + search_term.replace(" ", "+")
-
-    page = requests.get(search_url)
+    page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
     temp_soup = soup.find_all("div", class_="result")
 
-    final_result = []
+    result = []
 
     for div in temp_soup:
-        div_data = scrape_data_from_div(div)
-        final_result.append(div_data)
-    return final_result
+        item = scrape_data_from_div(div)
+        result.append(item)
+    return json.dumps(result)
 
-
-print(json.dumps(CiteSeerX("residual learning")))
