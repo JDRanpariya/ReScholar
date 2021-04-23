fbfa5baa00553da47220f170f1045e81a0c284ce
theUnrealSamurai <roshan.saravanan001@gmail.com>
2021-04-23 11:53:22 +0530

    Updated the citeseerxPaperScraper.py to scrape the common Attributes

--- data_sources/citeseerx/citeseerxPaperScraper.py
+++ data_sources/citeseerx/citeseerxPaperScraper.py
@@ -3,10 +3,6 @@ from bs4 import BeautifulSoup
 import json
 
 
-def scrape_data_from_div(soup_div):
-    item = {}
-    domain = "https://citeseerx.ist.psu.edu"
-
     # * TODO: extract following
     # ? Common Attributes
     # title
@@ -24,18 +20,33 @@ def scrape_data_from_div(soup_div):
     # versionsLink
     # relatedTopics
 
-    return item
-
-
 def CiteSeerXPaperScraper():
 
-    #  example url: https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.1090.7865&rank=1&q=residual%20learning&osm=&ossid=
-    url = request.args.get('link')
+    domain = "https://citeseerx.ist.psu.edu"
+    url = "https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.717.6419&rank=5&q=residual%20learning&osm=&ossid="
+    # url = request.args.get('link')
+
+    item = {}
 
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    item = scrape_data_from_div(soup)
+    item["title"] = soup.find("h2").text
 
-    return json.dumps(item)
+    authors_string = soup.find("div", id="docAuthors").text
+    authors_list = authors_string.split()
+    authors_list = ''.join(authors_list).split(',')
+    authors_list[0] = authors_list[0][2:]
+    
+    item["authors"] = authors_list
+    item["journal"] = soup.find("tr", id="docVenue").find_all("td")[1].text
+    item["year"] = int(soup.find("div", id="bibtex", class_="block").find("p").text[-6:-2])
+    item["snippet"] = soup.find("div", id="abstract").find("p").text
+    item["citations"] = soup.find("tr", id="docCites").find("a", title="number of citations").text.split()[0]
+    item["citationLink"] = domain + soup.find("div", id="docMenu", class_="submenu").find_all("a")[1]["href"]
+    item["deepLink"] = "None"
+    
+
+    print()
+
+CiteSeerXPaperScraper()
