19e80cd5ef90969ce113fd9777eee3991549e08b
jdranpariya <jdpatel.code@gmail.com>
2021-04-25 09:24:23 +0530

    updated MicrosoftAcademicTS

--- data_sources/microsoft_academic/index.js
+++ data_sources/microsoft_academic/index.js
@@ -31,131 +31,143 @@ exports.MicrosoftAcademicTS = function MicrosoftAcademicTS(req, res) {
       console.log(url);
       await page.goto(url, { waitUntil: "domcontentloaded" });
       console.log("Page downloaded");
-      let items = []
-      await page.evaluate(() => {
-        // * TODO
-        //  let items = [];
-        const results = document.querySelectorAll(
-          "#mainArea > router-view > ma-serp > div > div.results > div > compose > div > div.results > ma-card"
-        );
-        console.log(results);
+      const getResults = await page.evaluate(async () => {
+        return await new Promise(resolve => { // <-- return the data to node.js from browser
+          // scraping
+          let items = [];
+          const results = document.querySelectorAll(
+            "#mainArea > router-view > ma-serp > div > div.results > div > compose > div > div.results > ma-card"
+          );
+          console.log(results);
 
-        for (let result of results) {
-          console.log(result.querySelector("span")?.innerText ?? "");
-          items.push({
-            title: result.querySelector("span")?.innerText ?? "",
-            authors:
-              result.querySelector("div.authors")?.innerText?.split(",") ?? "",
-            journal:
-              result
-                .querySelector("a.publication")
-                ?.innerText?.trim()
-                .split(" ")
-                .slice(1, -1)
-                .join(" ")
-                .trim() ?? "",
-            year:
-              result
-                .querySelector("a.publication")
-                ?.innerText?.trim()
-                ?.split(" ")[0] ?? "",
-            snippets:
-              result.querySelector("div.text > span > span.au-target")
-                ?.innerText ?? "",
-            citations:
-              result
-                .querySelector("div > div.citation > a > span")
-                ?.innerText?.split(" ")[0] ?? "",
-            citationsLink:
-              result.querySelector("div > div.citation > a")?.href ?? "",
-            detailsLink: result.querySelector("a")?.href ?? "",
-          });
-        }
+          for (let result of results) {
+            console.log(result.querySelector("span")?.innerText ?? "");
+            items.push({
+              title: result.querySelector("span")?.innerText ?? "",
+              authors:
+                result.querySelector("div.authors")?.innerText?.split(",") ?? "",
+              journal:
+                result
+                  .querySelector("a.publication")
+                  ?.innerText?.trim()
+                  .split(" ")
+                  .slice(1, -1)
+                  .join(" ")
+                  .trim() ?? "",
+              year:
+                result
+                  .querySelector("a.publication")
+                  ?.innerText?.trim()
+                  ?.split(" ")[0] ?? "",
+              snippets:
+                result.querySelector("div.text > span > span.au-target")
+                  ?.innerText ?? "",
+              citations:
+                result
+                  .querySelector("div > div.citation > a > span")
+                  ?.innerText?.split(" ")[0] ?? "",
+              citationsLink:
+                result.querySelector("div > div.citation > a")?.href ?? "",
+              detailsLink: result.querySelector("a")?.href ?? "",
+            })
+          };
+          // * TODO
+          resolve(JSON.stringify({ items: items })); // res.status(200).send(JSON.stringify({ items: items })); //JSON.stringify(getResults);
 
-        // res.status(200).send(JSON.stringify({ items: items })); //JSON.stringify(getResults);
+        });
       });
-      res.status(200).send(JSON.stringify({ items: items }));
+      res.status(200).send(getResults);
       await browser.close();
     }
 
     // get paper details function
-    async function getPaperDetails() {
+
+
+    async function getSearchResults() {
       const parse_url = req.query.url;
       const url = parse_url;
       console.log(url);
       await page.goto(url, { waitUntil: "domcontentloaded" });
       console.log("page downloaded paper details")
-      const getDetail = page.evaluate(() => {
-        const title =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > h1"
-          )?.innerText ?? "";
-        const year =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > a.au-target.publication > span.year"
-          )?.innerText ?? "";
-        const references =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > div.stats > ma-statistics-item:nth-child(1) > div.ma-statistics-item.au-target > div.data > div.count"
-          )?.innerText ?? "";
-        const citations =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > div.stats > ma-statistics-item:nth-child(2) > div.ma-statistics-item.au-target > div.data > div.count"
-          )?.innerText ?? "";
-        const doi = document
-          .querySelector(
-            "#mainArea > router-view > div > div > div > div > a.doiLink.au-target"
-          )
-          .innerText.split(" ")[2];
-        const journal =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > a.au-target.publication > span.pub-name.au-target"
-          )?.innerText ??
-          "" ??
-          "";
-        // get authors
-        let authors = [];
-        const authorsNodeList = document.querySelectorAll(
-          "#mainArea > router-view > div > div > div > div > ma-author-string-collection > div > div > div"
-        );
-        for (let author of [...authorsNodeList]) {
-          authors.push(author?.children[0]?.innerText) ?? "";
-        }
-        // abstract
-        const abstractText =
-          document.querySelector(
-            "#mainArea > router-view > div > div > div > div > p"
-          )?.innerText ?? "";
-        // references, cited by and related links
-        const referencesLink = document?.URL ?? "";
-        const citationsLink =
-          referencesLink?.replace("reference", "citedby") ?? "";
-        const relatedLink = referencesLink?.replace("reference", "related") ?? "";
-        // * TODO
-        const links = [];
-        const pdfLinks = [];
+      const getDetails = await page.evaluate(async () => {
+        return await new Promise(resolve => { // <-- return the data to node.js from browser
+          // scraping
+          const results = document.querySelectorAll(
+            "#mainArea > router-view > ma-serp > div > div.results > div > compose > div > div.results > ma-card"
+          );
+          console.log(results);
 
-        return {
-          // title: title,
-          // authors: authors,
-          // journal: journal,
-          // year: year,
-          // citations: citations,
-          // citationsLink: citationsLink,
-          doi: doi,
-          abstractText: abstractText,
-          links: links,
-          pdfLinks: pdfLinks,
-          references: references,
-          referencesLinks: referencesLink,
-          relatedLink: relatedLink,
-        };
+          // const title =
+          //   document.querySelector(
+          //     "#mainArea > router-view > div > div > div > div > h1"
+          //   )?.innerText ?? "";
+          // const year =
+          //   document.querySelector(
+          //     "#mainArea > router-view > div > div > div > div > a.au-target.publication > span.year"
+          //   )?.innerText ?? "";
+          const references =
+            document.querySelector(
+              "#mainArea > router-view > div > div > div > div > div.stats > ma-statistics-item:nth-child(1) > div.ma-statistics-item.au-target > div.data > div.count"
+            )?.innerText ?? "";
+          // const citations =
+          //   document.querySelector(
+          //     "#mainArea > router-view > div > div > div > div > div.stats > ma-statistics-item:nth-child(2) > div.ma-statistics-item.au-target > div.data > div.count"
+          //   )?.innerText ?? "";
+          const doi = document
+            .querySelector(
+              "#mainArea > router-view > div > div > div > div > a.doiLink.au-target"
+            )
+            .innerText.split(" ")[2];
+          // const journal =
+          //   document.querySelector(
+          //     "#mainArea > router-view > div > div > div > div > a.au-target.publication > span.pub-name.au-target"
+          //   )?.innerText ??
+          //   "" ??
+          //   "";
+          // get authors
+          // let authors = [];
+          // const authorsNodeList = document.querySelectorAll(
+          //   "#mainArea > router-view > div > div > div > div > ma-author-string-collection > div > div > div"
+          // );
+          // for (let author of [...authorsNodeList]) {
+          //   authors.push(author?.children[0]?.innerText) ?? "";
+          // }
+          // abstract
+          const abstractText =
+            document.querySelector(
+              "#mainArea > router-view > div > div > div > div > p"
+            )?.innerText ?? "";
+          // references, cited by and related links
+          const referencesLink = document?.URL ?? "";
+          // const citationsLink =
+          //   referencesLink?.replace("reference", "citedby") ?? "";
+          const relatedLink = referencesLink?.replace("reference", "related") ?? "";
+          // * TODO
+          const links = [];
+          const pdfLinks = [];
+          // * TODO
+          resolve({
+            // title: title,
+            // authors: authors,
+            // journal: journal,
+            // year: year,
+            // citations: citations,
+            // citationsLink: citationsLink,
+            doi: doi,
+            abstractText: abstractText,
+            links: links,
+            pdfLinks: pdfLinks,
+            references: references,
+            referencesLinks: referencesLink,
+            relatedLink: relatedLink,
+          }); // res.status(200).send(JSON.stringify({ items: items })); //JSON.stringify(getResults);
+
+        });
       });
-
-      await res.status(200).send(JSON.stringify(await getDetail()));
-
+      res.status(200).send(JSON.stringify(await getDetails()));
       await browser.close();
     }
   }
 }
+
 
