1282d6db62492e89116713ee552d54ef41645c3e
Pranav Abraham <mailforpranav247@gmail.com>
2021-04-27 12:20:51 +0300

    Implemented a v1 NavigationDrawer

--- frontend/lib/widgets/navigation_drawer.dart
+++ frontend/lib/widgets/navigation_drawer.dart
@@ -1,7 +1,9 @@
-import 'package:expandable/expandable.dart';
+import 'package:flutter_treeview/tree_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:rescholar/models/custom_icons.dart';
+import 'package:rescholar/data/user_library.dart';
 import 'package:rescholar/widgets/custom_toast.dart';
 import 'package:rescholar/models/rescholar_user.dart';
 
@@ -12,6 +14,92 @@ class NavigationDrawer extends StatefulWidget {
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
   String _selection = 'All Papers';
+  Map<String, int> libraryItemCount = userLibrary['libraryItemCount'];
+  TreeViewController _treeViewController = TreeViewController();
+
+  TreeViewTheme _treeViewTheme = TreeViewTheme(
+    expanderTheme: ExpanderThemeData(
+      type: ExpanderType.chevron,
+      position: ExpanderPosition.start,
+      color: Color(0xFFB2B2B2),
+      size: 28.0,
+    ),
+    labelStyle: TextStyle(
+      fontSize: 16.0,
+      color: Colors.white,
+    ),
+    parentLabelStyle: TextStyle(
+      fontSize: 16.0,
+      color: Colors.white,
+    ),
+    colorScheme: ColorScheme.dark(),
+  );
+  _expandNode(String key, bool expanded) {
+    {
+      String msg = '${expanded ? "Expanded" : "Collapsed"}: $key';
+      debugPrint(msg);
+      Node node = _treeViewController.getNode(key);
+      if (node != null) {
+        List<Node> updated;
+        updated = _treeViewController.updateNode(
+            key, node.copyWith(expanded: expanded));
+        setState(() {
+          _treeViewController = _treeViewController.copyWith(children: updated);
+        });
+      }
+    }
+  }
+
+  _nodeBuilder(BuildContext context, Node<dynamic> node) {
+    return Container(
+      margin: EdgeInsets.only(right: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(15.0),
+        ),
+        color: Color(0x4D21629D),
+      ),
+      child: ListTile(
+          onTap: () {
+            setState(() {
+              _selection = node.label;
+            });
+          },
+          onLongPress: () {},
+          selected: _selection == node.label,
+          tileColor: Color(0xFF1C1C1C),
+          selectedTileColor: Colors.transparent,
+          leading: node.key == "ROOT"
+              ? Padding(
+                  padding: const EdgeInsets.only(left: 2.0, right: 6.0),
+                  child: Icon(
+                    CustomIcons.mdiFolderHome,
+                    color: Color(node.data["folderColour"]),
+                    size: 20.0,
+                  ),
+                )
+              : Icon(
+                  Icons.folder_rounded,
+                  color: Color(node.data["folderColour"]),
+                  size: 28.0,
+                ),
+          minLeadingWidth: 0.0,
+          horizontalTitleGap: 10.0,
+          title: Text(node.label,
+              style: TextStyle(fontSize: 16.0, color: Colors.white)),
+          trailing: Text("${node.data["itemCount"]}",
+              style: TextStyle(fontSize: 14.0, color: Color(0xFFB2B2B2))),
+          contentPadding: EdgeInsets.only(left: 8.0, right: 16.0),
+          visualDensity: VisualDensity(vertical: -3.0)),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final List<Map<String, dynamic>> folderStruct = userLibrary['folderModel'];
+    _treeViewController = _treeViewController.loadMap(list: folderStruct);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +124,8 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                         ),
                       ),
                       // TODO: Write logic for getting the item count inside each library sub-sections
-                      // TODO: Implement onTap routing to different pages
-                      // TODO: Implement onLongPress menu & logic
+                      // TODO: Implement onTap routing to library sections
+                      // TODO: Implement onLongPress menu & logic for folders sections
                       Container(
                         margin: EdgeInsets.symmetric(
                             horizontal: 5.0, vertical: 0.0),
@@ -66,7 +154,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             title: Text("All Papers",
                                 style: TextStyle(
                                     fontSize: 16.0, color: Colors.white)),
-                            trailing: Text("30",
+                            trailing: Text("${libraryItemCount["All Papers"]}",
                                 style: TextStyle(
                                     fontSize: 14.0, color: Color(0xFFB2B2B2))),
                             visualDensity: VisualDensity(vertical: -3.0)),
@@ -104,7 +192,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             title: Text("Favourites",
                                 style: TextStyle(
                                     fontSize: 16.0, color: Colors.white)),
-                            trailing: Text("0",
+                            trailing: Text("${libraryItemCount["Favourites"]}",
                                 style: TextStyle(
                                     fontSize: 14.0, color: Color(0xFFB2B2B2))),
                             visualDensity: VisualDensity(vertical: -3.0)),
@@ -139,7 +227,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             title: Text("Archive",
                                 style: TextStyle(
                                     fontSize: 16.0, color: Colors.white)),
-                            trailing: Text("0",
+                            trailing: Text("${libraryItemCount["Archive"]}",
                                 style: TextStyle(
                                     fontSize: 14.0, color: Color(0xFFB2B2B2))),
                             visualDensity: VisualDensity(vertical: -3.0)),
@@ -174,7 +262,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             title: Text("Recycle Bin",
                                 style: TextStyle(
                                     fontSize: 16.0, color: Colors.white)),
-                            trailing: Text("0",
+                            trailing: Text("${libraryItemCount["Recycle Bin"]}",
                                 style: TextStyle(
                                     fontSize: 14.0, color: Color(0xFFB2B2B2))),
                             visualDensity: VisualDensity(vertical: -3.0)),
@@ -187,11 +275,14 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                         endIndent: 12.0,
                       ),
                       Expanded(
-                          child: ListView(
-                        children: [
-                          // ExpandablePanel(collapsed: collapsed, expanded: expanded)
-                        ],
-                      ))
+                        child: TreeView(
+                          controller: _treeViewController,
+                          allowParentSelect: false,
+                          onExpansionChanged: _expandNode,
+                          theme: _treeViewTheme,
+                          nodeBuilder: _nodeBuilder,
+                        ),
+                      )
                     ],
                   )))),
     );
