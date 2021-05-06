f5d4ff671b4e9811c4c6481b2e6d4378ce9cfc75
Pranav Abraham <mailforpranav247@gmail.com>
2021-05-06 10:25:18 +0300

    Fixed HTTP call Added proper arguments for fetching widget

--- frontend/lib/services/re_search_engine.dart
+++ frontend/lib/services/re_search_engine.dart
@@ -1,27 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-import 'package:rescholar/models/papers_list.dart';
-import 'package:rescholar/utils/paper_functions.dart';
 
 /// The search engine that makes HTTP requests to the Google Cloud backend to
 /// retrieve search results from a variety of sources supported by ReScholar.
 class ReSearchEngine {
   /// Retrieves search results from the GoogleScholar Google Cloud Function.
-  Future<PapersList> fetchFromGoogleScholar(query, paperCount) async {
+  static Future fetchSearchResults(
+      String source, String service, String query, int paperCount) async {
     final response = await http.get(Uri.https(
         'europe-west1-rescholar-ltwk.cloudfunctions.net',
-        'GoogleScholar?q=${query}&paper_count=${paperCount}'));
+        '$source',
+        {"svc": service, "q": query, "paper_count": "$paperCount"}));
 
     if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      debugPrint("DEBUG: " + response.body);
-      return getPapersListFromJSON(response.body);
+      return jsonDecode(response.body);
     } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load Papers');
+      throw Exception("ERROR: Failed to fetch search results");
     }
   }
 }
