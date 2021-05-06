93235cbd851ea0a74735e2b5762ad03dec100517
Pranav Abraham <mailforpranav247@gmail.com>
2021-05-06 10:29:27 +0300

    Refactored buttons.

--- frontend/lib/widgets/header.dart
+++ frontend/lib/widgets/header.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
-import 'package:rescholar/widgets/buttons.dart';
-
 /// Builds an AppBar with a leading icon, title and an optional local search button.
 class Header extends StatelessWidget implements PreferredSizeWidget {
   final Icon leadingIcon;
@@ -27,7 +25,17 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
         toolbarHeight: 75,
         automaticallyImplyLeading: false,
         leadingWidth: 50.0,
-        leading: NavigationDrawerButton(leadingIcon, leadingIconGradient),
+        leading: GestureDetector(
+          onTap: () => Scaffold.of(context).openDrawer(),
+          child: ShaderMask(
+            shaderCallback: (rect) => LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: leadingIconGradient,
+            ).createShader(rect),
+            child: leadingIcon,
+          ),
+        ),
         title: ShaderMask(
           shaderCallback: (rect) => LinearGradient(
                   begin: Alignment.centerLeft,
@@ -47,7 +55,20 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
             ? <Widget>[
                 Padding(
                   padding: EdgeInsets.only(top: 2.0),
-                  child: SearchButton(searchGradient),
+                  child: GestureDetector(
+                    onTap: () {/* TODO: Implement Search Button logic */},
+                    child: ShaderMask(
+                      shaderCallback: (rect) => LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: searchGradient,
+                      ).createShader(rect),
+                      child: Icon(
+                        Icons.search_rounded,
+                        size: 54,
+                      ),
+                    ),
+                  ),
                 )
               ]
             : null,
