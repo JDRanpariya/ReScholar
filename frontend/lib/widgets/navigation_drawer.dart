81d84b0904f989c75b854295a5f97a6557a71c7c
Pranav Abraham <mailforpranav247@gmail.com>
2021-05-15 19:29:01 +0300

    Added expansion memory

--- frontend/lib/widgets/navigation_drawer.dart
+++ frontend/lib/widgets/navigation_drawer.dart
@@ -1,8 +1,9 @@
-import 'package:flutter_treeview/tree_view.dart';
+import 'package:flutter_treeview/flutter_treeview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:collection/collection.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 
 import 'package:rescholar/widgets/custom_toast.dart';
 import 'package:rescholar/models/rescholar_user.dart';
@@ -13,113 +14,131 @@ import 'package:rescholar/models/user_library.dart';
 /// Builds a [NavigationDrawer] that enables routing between the primary sections
 /// of the Library and the [TreeView]-based Folder Tree.
 class NavigationDrawer extends StatefulWidget {
+  String selection;
+
+  NavigationDrawer({Key key, this.selection}) : super(key: key);
+
   @override
   _NavigationDrawerState createState() => _NavigationDrawerState();
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
-  String _selection = 'Papers';
   TreeViewController _treeViewController = TreeViewController();
 
-  TreeViewTheme _treeViewTheme = TreeViewTheme(
-    expanderTheme: ExpanderThemeData(
-      type: ExpanderType.chevron,
-      position: ExpanderPosition.start,
-      color: Color(0xFFB2B2B2),
-      size: 28.0,
-    ),
-    labelStyle: TextStyle(
-      fontSize: 16.0,
-      color: Colors.white,
-    ),
-    parentLabelStyle: TextStyle(
-      fontSize: 16.0,
-      color: Colors.white,
-    ),
-    colorScheme: ColorScheme.dark(),
-  );
-  _expandNode(String key, bool expanded) {
-    {
-      String msg = '${expanded ? "Expanded" : "Collapsed"}: $key';
-      debugPrint(msg);
-      Node node = _treeViewController.getNode(key);
-      if (node != null) {
-        List<Node> updated;
-        updated = _treeViewController.updateNode(
-            key, node.copyWith(expanded: expanded));
-        setState(() {
-          _treeViewController = _treeViewController.copyWith(children: updated);
-        });
-      }
-    }
-  }
-
-  _nodeBuilder(BuildContext context, Node<dynamic> node) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(15.0),
-      child: ListTile(
-          onTap: () {
-            setState(() {
-              _selection = node.label;
-              Navigator.pop(context);
-              Navigator.pushNamedAndRemoveUntil(context, "/library_folders",
-                  ModalRoute.withName("/library_papers"),
-                  arguments: {
-                    "header": Header(
-                        Icon(
-                          node.key == "ROOT/"
-                              ? MdiIcons.folderHome
-                              : Icons.folder_rounded,
-                          size: 54.0,
-                        ),
-                        [
-                          const Color(0xFF3D6BB8),
-                          const Color(0xFF738EBC),
-                        ],
-                        "${node.label.toLowerCase()}",
-                        [
-                          const Color(0xFF738EBC),
-                          const Color(0xFFE2EDFF),
-                        ],
-                        true,
-                        [
-                          const Color(0xFF9DD0FF),
-                          const Color(0xFF4880DE),
-                        ]),
-                    "folderBar": FolderBar(selectedFolderKey: node.key),
-                    "librarySection": node.key
-                  });
-            });
-          },
-          onLongPress: () {},
-          selected: _selection == node.label,
-          tileColor: Color(0xFF1C1C1C),
-          selectedTileColor: Color(0x4D21629D),
-          leading: node.key == "ROOT/"
-              ? Icon(
-                  MdiIcons.folderHome,
-                  color: Color(node.data["folderColour"]),
-                  size: 28.0,
-                )
-              : Icon(
-                  Icons.folder_rounded,
-                  color: Color(node.data["folderColour"]),
-                  size: 28.0,
-                ),
-          minLeadingWidth: 0.0,
-          horizontalTitleGap: 10.0,
-          title: Text(node.label,
-              style: TextStyle(fontSize: 16.0, color: Colors.white)),
-          trailing: Text("${node.data["paperCount"]}",
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFB2B2B2))),
-          contentPadding: EdgeInsets.only(left: 8.0, right: 16.0),
-          visualDensity: VisualDensity(vertical: -3.0)),
-    );
+  @override
+  void initState() {
+    super.initState();
+    _treeViewController = _treeViewController.loadMap(
+        list: context.read<UserLibrary>().folderTree);
   }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<ReScholarUser>(context);
+
+    TreeViewTheme _treeViewTheme = TreeViewTheme(
+      horizontalSpacing: 19.0,
+      expanderTheme: ExpanderThemeData(
+        type: ExpanderType.chevron,
+        position: ExpanderPosition.start,
+        color: Color(0xFFB2B2B2),
+        size: 28.0,
+      ),
+      labelStyle: TextStyle(
+        fontSize: 16.0,
+        color: Colors.white,
+      ),
+      parentLabelStyle: TextStyle(
+        fontSize: 16.0,
+        color: Colors.white,
+      ),
+      colorScheme: ColorScheme.dark(),
+    );
+
+    _expandNode(String key, bool expanded) {
+      {
+        debugPrint('${expanded ? "Expanded" : "Collapsed"}: $key');
+        Node node = _treeViewController.getNode(key);
+        if (node != null) {
+          List<Node> updated;
+          updated = _treeViewController.updateNode(
+              key, node.copyWith(expanded: expanded));
+          setState(() {
+            _treeViewController =
+                _treeViewController.copyWith(children: updated);
+            context
+                .read<UserLibrary>()
+                .updateFolderTree(_treeViewController.asMap);
+          });
+        }
+      }
+    }
+
+    Widget _nodeBuilder(BuildContext context, Node<dynamic> node) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(15.0),
+        child: ListTile(
+            onTap: () {
+              setState(() {
+                widget.selection = node.label;
+                Navigator.pop(context);
+                Navigator.pushNamedAndRemoveUntil(context, "/library_folders",
+                    ModalRoute.withName("/library_papers"),
+                    arguments: {
+                      "header": Header(
+                          Icon(
+                            node.key == "ROOT/"
+                                ? MdiIcons.folderHome
+                                : Icons.folder_rounded,
+                            size: 54.0,
+                          ),
+                          [
+                            const Color(0xFF3D6BB8),
+                            const Color(0xFF738EBC),
+                          ],
+                          "${node.label}",
+                          [
+                            const Color(0xFF738EBC),
+                            const Color(0xFFE2EDFF),
+                          ],
+                          true,
+                          [
+                            const Color(0xFF9DD0FF),
+                            const Color(0xFF4880DE),
+                          ]),
+                      "folderBar": FolderBar(selectedFolderKey: node.key),
+                      "folderKey": node.key
+                    });
+              });
+            },
+            onLongPress: () {},
+            selected: widget.selection == node.label,
+            tileColor: Color(0xFF1C1C1C),
+            selectedTileColor: Color(0x4D21629D),
+            leading: node.key == "ROOT/"
+                ? Icon(
+                    MdiIcons.folderHome,
+                    color: Color(node.data["folderColour"]),
+                    size: 28.0,
+                  )
+                : Icon(
+                    Icons.folder_rounded,
+                    color: Color(node.data["folderColour"]),
+                    size: 28.0,
+                  ),
+            minLeadingWidth: 0.0,
+            horizontalTitleGap: 10.0,
+            title: Text(node.label,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                softWrap: true,
+                style: TextStyle(fontSize: 16.0, color: Colors.white)),
+            trailing: Text("${node.data["paperCount"]}",
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFB2B2B2))),
+            contentPadding: EdgeInsets.only(left: 8.0, right: 16.0),
+            visualDensity: VisualDensity(vertical: -3.0)),
+      );
+    }
 
     return SafeArea(
       child: ClipRRect(
@@ -146,15 +165,39 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                         child: ListTile(
                             onTap: () {
                               setState(() {
-                                _selection = 'Papers';
+                                widget.selection = 'Papers';
                                 Navigator.pop(context);
-                                Navigator.popUntil(
-                                  context,
-                                  ModalRoute.withName("/library_papers"),
-                                );
+                                // Navigator.popUntil(
+                                //   context,
+                                //   ModalRoute.withName("/library_papers"),
+                                // );
+                                Navigator.pushNamedAndRemoveUntil(
+                                    context, "/library_papers", (_) => false,
+                                    arguments: {
+                                      "header": Header(
+                                          Icon(
+                                            FluentIcons.library_20_filled,
+                                            size: 54,
+                                          ),
+                                          [
+                                            const Color(0xFFFFA740),
+                                            const Color(0xFFFFCA8B),
+                                          ],
+                                          "Papers",
+                                          [
+                                            const Color(0xFFFFC27A),
+                                            const Color(0xFF8BB6FF),
+                                          ],
+                                          true,
+                                          [
+                                            const Color(0xFF9DD0FF),
+                                            const Color(0xFF4880DE),
+                                          ]),
+                                      "renderGreeting": true,
+                                    });
                               });
                             },
-                            selected: _selection == 'Papers',
+                            selected: widget.selection == 'Papers',
                             tileColor: Color(0xFF1C1C1C),
                             selectedTileColor: Color(0x4D21629D),
                             leading: Icon(
@@ -188,7 +231,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             onTap: () {
                               if (user.isAnonymous == false) {
                                 setState(() {
-                                  _selection = 'Favourites';
+                                  widget.selection = 'Favourites';
                                   Navigator.pop(context);
                                   Navigator.pushNamedAndRemoveUntil(
                                       context,
@@ -204,7 +247,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFFFFC000),
                                               const Color(0xFFFFE28A),
                                             ],
-                                            "favourites",
+                                            "Favourites",
                                             [
                                               const Color(0xFFFFE28A),
                                               const Color(0xFF9DD0FF),
@@ -214,7 +257,6 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFF9DD0FF),
                                               const Color(0xFF4880DE),
                                             ]),
-                                        "librarySection": "Favourites"
                                       });
                                 });
                               } else {
@@ -222,7 +264,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                     "Please sign in with Google to view this page");
                               }
                             },
-                            selected: _selection == 'Favourites',
+                            selected: widget.selection == 'Favourites',
                             tileColor: Color(0xFF1C1C1C),
                             selectedTileColor: Color(0x4D21629D),
                             leading: Icon(
@@ -256,7 +298,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             onTap: () {
                               if (user.isAnonymous == false) {
                                 setState(() {
-                                  _selection = 'Archive';
+                                  widget.selection = 'Archive';
                                   Navigator.pop(context);
                                   Navigator.pushNamedAndRemoveUntil(
                                       context,
@@ -272,7 +314,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFFFF9536),
                                               const Color(0xFFFFBD82),
                                             ],
-                                            "archive",
+                                            "Archive",
                                             [
                                               const Color(0xFFFFBD82),
                                               const Color(0xFF9DD0FF),
@@ -282,7 +324,6 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFF9DD0FF),
                                               const Color(0xFF4880DE),
                                             ]),
-                                        "librarySection": "Archive"
                                       });
                                 });
                               } else {
@@ -290,7 +331,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                     "Please sign in with Google to view this page");
                               }
                             },
-                            selected: _selection == 'Archive',
+                            selected: widget.selection == 'Archive',
                             tileColor: Color(0xFF1C1C1C),
                             selectedTileColor: Color(0x4D21629D),
                             leading: Icon(Icons.archive_rounded,
@@ -321,10 +362,10 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                             onTap: () {
                               if (user.isAnonymous == false) {
                                 setState(() {
-                                  _selection = 'Recycle Bin';
+                                  widget.selection = 'Recycle Bin';
                                   Navigator.pop(context);
                                   Navigator.pushNamed(
-                                      context, "library_recycle_bin",
+                                      context, "/library_recycle_bin",
                                       arguments: {
                                         "header": Header(
                                             Icon(
@@ -335,7 +376,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFFEB5757),
                                               const Color(0xFFE98787),
                                             ],
-                                            "recycle bin",
+                                            "Recycle Bin",
                                             [
                                               const Color(0xFFE98787),
                                               const Color(0xFF9DD0FF),
@@ -345,7 +386,6 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                               const Color(0xFF9DD0FF),
                                               const Color(0xFF4880DE),
                                             ]),
-                                        "librarySection": "Recycle Bin"
                                       });
                                 });
                               } else {
@@ -353,7 +393,7 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                                     "Please sign in with Google to view this page");
                               }
                             },
-                            selected: _selection == 'Recycle Bin',
+                            selected: widget.selection == 'Recycle Bin',
                             tileColor: Color(0xFF1C1C1C),
                             selectedTileColor: Color(0x4D21629D),
                             leading: Icon(Icons.delete_rounded,
@@ -385,24 +425,14 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
                         indent: 12.0,
                         endIndent: 12.0,
                       ),
-                      Selector<UserLibrary, List<Map<String, dynamic>>>(
-                          selector: (context, userLibrary) =>
-                              userLibrary.folderTree,
-                          shouldRebuild: (listOfMaps1, listOfMaps2) =>
-                              DeepCollectionEquality()
-                                  .equals(listOfMaps1, listOfMaps2),
-                          builder: (context, data, child) {
-                            return Expanded(
-                              child: TreeView(
-                                controller:
-                                    _treeViewController.loadMap(list: data),
-                                allowParentSelect: false,
-                                onExpansionChanged: _expandNode,
-                                theme: _treeViewTheme,
-                                nodeBuilder: _nodeBuilder,
-                              ),
-                            );
-                          }),
+                      Expanded(
+                        child: TreeView(
+                          controller: _treeViewController,
+                          onExpansionChanged: _expandNode,
+                          theme: _treeViewTheme,
+                          nodeBuilder: _nodeBuilder,
+                        ),
+                      ),
                     ],
                   )))),
     );
